f51c6d3599becb60d1b0e90e768a11606ed94dbb
walter brunetti <walterbrunetti@gmail.com>
2015-11-27 13:59:29 -0300

    cambios sitio

--- sendMail.py
+++ sendMail.py
@@ -1,39 +1,25 @@
 import smtplib
+from django.conf import settings
 from django.http import HttpResponse
 from django.template import Template, Context, loader
 
+
 def sendMail(request):
-    
-    toaddrs = ['cedirsalud@gmail.com', 'walterbrunetti@gmail.com',
-               'jose.brunetti@gmail.com', 'recepcioncedir@gmail.com']
-    #toaddrs = 'walterbrunetti@gmail.com'
+    import pdb; pdb.set_trace()
+    toaddrs = settings.EMAIL_NOTIFICATION_ACCOUNTS
     subject = "Subject: Nuevo mensaje registrado desde cedirsalud.com.ar\n\n"
     request.content_type = "text/html"
     
-    #fromaddr = 'cedirsalud@gmail.com'
-    gmail_user = 'cedirsalud@gmail.com'
-    gmail_pwd = 'endocapsula'
+    gmail_user = settings.EMAIL_ACCOUNT_USER
+    gmail_pwd = settings.EMAIL_ACCOUNT_PSW
 
-
-    
-    
-    
     name = request.POST['name']
     email = request.POST['email']
     tel = request.POST['tel']
     message = request.POST['message']
 
-    header = 'To:' + 'cedirsalud@gmail.com,walterbrunetti@gmail.com' + '\n' + 'From: ' + gmail_user + '\n' + subject
     msg = subject + 'Nombre: ' + name + "\n" + 'Mail: ' + email + "\n" + 'Tel: ' + tel + "\n" + 'Mensaje: ' + message + "\n"
-    #msg = header + '\n this is test msg \n\n'
 
-    
-    #server = smtplib.SMTP('localhost')
-    #server.sendmail(fromaddr, toaddrs, msg)
-    #server.quit()
-    
-    
-    
     smtpserver = smtplib.SMTP("smtp.gmail.com",587)
     smtpserver.ehlo()
     smtpserver.starttls()
@@ -42,13 +28,12 @@ def sendMail(request):
     smtpserver.sendmail(gmail_user, toaddrs, msg)
     smtpserver.close()
     
-    
-    
     templateName = 'contacto_ok.html'
     t = loader.get_template('pages/' + templateName)
     
     c = Context({
         #'latest_poll_list': latest_poll_list,
-	#'current_date': now,
+	    #'current_date': now,
     })
     return HttpResponse(t.render(c))
+
