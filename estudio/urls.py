dd24e2853166fbb48cd2d87f6e036f9e281da69c
José Ignacio Aguilera <jiaguilera@gmail.com>
2016-08-10 21:10:21 -0300

    permite estudios multipáginas

--- estudio/urls.py
+++ estudio/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
-from estudio.views import imprimir
+from estudio.views import imprimir, imprimir_nuevo
 
 urlpatterns = patterns('',
     url(r'^estudio/(?P<id_estudio>\d+)/imprimir/$', imprimir, name='estudio_imprimir'),
+    url(r'^estudio/(?P<id_estudio>\d+)/imprimir/nuevo/$', imprimir_nuevo, name='estudio_imprimir'),
 )
 
--- estudio/views.py
+++ estudio/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from estudio.models import Estudio
 from imprimir import generar_informe
+from imprimir_nuevo import generar_informe_nuevo
 
 
 def imprimir(request, id_estudio):
@@ -13,3 +14,14 @@ def imprimir(request, id_estudio):
     response['Content-Disposition'] = u'filename="Estudio de {0}.pdf"'.format(estudio.paciente.apellido)
 
     return generar_informe(response, estudio)
+
+
+def imprimir_nuevo(request, id_estudio):
+
+    estudio = Estudio.objects.get(pk=id_estudio)
+
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = u'filename="Estudio de {0}.pdf"'.format(estudio.paciente.apellido)
+
+    return generar_informe_nuevo(response, estudio)
