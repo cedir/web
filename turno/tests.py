ce0ea47cc7d243ef85c8aac455230fc895f4d583
Walter Brunetti <walterbrunetti@gmail.com>
2016-09-25 12:36:10 -0300

    se arregla test

--- turno/tests.py
+++ turno/tests.py
@@ -1,36 +1,30 @@
 from django.test import TestCase
 from django.test import Client
-from django.http import HttpRequest
 from django.contrib.auth.models import AnonymousUser, User
 from views import anunciar
 from estudio.models import Estudio
 from turno.models import Turno
 
 
-
 class TurnosTest(TestCase):
     fixtures = ['turnos.json']
 
     def setUp(self):
+        self.user = User.objects.create_user(username='walter', password='xx11', is_superuser=True)
         self.client = Client(HTTP_POST='localhost')
-        self.client.login(username='walter', password='12345')
-
-        self.user = User.objects.create(username="walter")
-        self.request = HttpRequest()
-        self.request.user = self.user
+        self.client.login(username='walter', password='xx11')
 
     def test_anunciar_truno(self):
         """
-        Deberia crear un estudio por cada practica del turno
+        Deberia crear un estudio por cada practica del turno.
+        TODO: para que este test corra hay que poner que el estudio se cree con idFacturacion = 1.
+        Los fixtures fallan porque no permite crear una presentacion con id = 0 y fallan los fereign keys.
         """
 
         self.assertEqual(Estudio.objects.all().count(), 0)
         turno = Turno.objects.get(pk=1)
         
-        #response = self.client.post('turno/1/anunciar/', {})  # TODO: esto es ideal en vez de llamar anunciar, pero no esta andando.
-        #print response
-        
-        response = anunciar(self.request, turno.id)
+        response = self.client.post('/turno/1/anunciar/', {})
         self.assertContains(response, '{"status": true, "message": "Success"}')
 
         self.assertEqual(Estudio.objects.all().count(), turno.practicas.all().count())
