45bff3704a2938c87b20a42eb5d803d84c6e89e2
Ignacio Aguilera <jiaguilera@gmail.com>
2017-01-16 21:38:23 -0300

    aplicacion de caja

---
+++ caja/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from medico.models import Medico
+from estudio.models import Estudio
+
+
+class TipoMovimientoCaja(models.Model):
+    id = models.IntegerField(primary_key=True)
+    descripcion = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tblCajaTipoDeMovimientos'
+
+# Create your models here.
+class MovimientoCaja(models.Model):
+    concepto = models.TextField(blank=True)
+    monto = models.DecimalField(max_digits=14, decimal_places=2, blank=True, null=True)
+    fecha = models.DateField(blank=True, null=True)
+    hora = models.TimeField()
+    tipo = models.ForeignKey(TipoMovimientoCaja, db_column='idTipoDeMovimiento', blank=True, null=True)
+    estudio = models.ForeignKey(Estudio, db_column='nroEstudio', related_name='movimientos_caja', blank=True, null=True)
+    medico = models.ForeignKey(Medico, db_column='idMedico', blank=True, null=True)
+    monto = models.DecimalField(db_column='montoAcumulado', max_digits=10, decimal_places=2, blank=True, null=True)  # Field name made lowercase.
+    estado = models.NullBooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'tblCajaMovimientos'
