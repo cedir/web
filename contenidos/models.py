baf8f8f03bf872836adcc16dce9d6570a71b1fde
Walter Brunetti <walterbrunetti@gmail.com>
2017-02-10 20:03:13 -0300

    Se arregla mapa

--- contenidos/models.py
+++ contenidos/models.py
@@ -11,13 +11,9 @@ class Categoria(models.Model):
     name = models.CharField("Nombre",max_length=200, null=False,blank=False)
     description = models.TextField("Descripcion",blank=True)
     friendlyURL = models.CharField("Friendly URL", max_length=100,null=True,blank=True)
-    
+
     def __unicode__ (self):
         return self.name
-
-    class Admin:
-        list_display = ('name', 'description')
-        search_fields = ['name']
 
 
 class Contenido(models.Model):
@@ -41,17 +37,17 @@ class Contenido(models.Model):
     @property
     def url(self):
         return '/content/{}'.format(self.friendlyURL) if self.friendlyURL else '/content/{}'.format(self.id)
-    
+
     def save(self):
         super(Contenido, self).save()
 
         minSizes = [147,140]
         medSizes = [200,200]
 
-        #f = open('/tmp/w.w','w')
         #TODO: checkear que no hay otra imagen con el mismo nombre y renomabrar, si es que no lo hace PIL automaticamente
 
         if self.img1.name <> '':
+            import ipdb; ipdb.set_trace()
             imgPath = settings.MEDIA_ROOT + self.img1.name
             im = Image.open(imgPath)
             filePathName, ext = os.path.splitext(imgPath)
@@ -69,33 +65,27 @@ class Contenido(models.Model):
             #    imCopy.thumbnail(medSizes, Image.ANTIALIAS)
             #    imCopy.save(filePathName + '_med' + ext, "JPEG")
 
-        if self.img2.name <> '':
-            imgPath = settings.MEDIA_ROOT + self.img2.name
-            im = Image.open(imgPath)
-            filePathName, ext = os.path.splitext(imgPath)
-            imgSize = im.size
-
-            #med image
-            if (imgSize[0] > medSizes[0]) or (imgSize[1] > medSizes[1]):
-                im.thumbnail(medSizes, Image.ANTIALIAS)
-                im.save(filePathName + '_med' + ext, "JPEG")
-
-        if self.img3.name <> '':
-            imgPath = settings.MEDIA_ROOT + self.img3.name
-            im = Image.open(imgPath)
-            filePathName, ext = os.path.splitext(imgPath)
-            imgSize = im.size
-
-            #med image
-            if (imgSize[0] > medSizes[0]) or (imgSize[1] > medSizes[1]):
-                im.thumbnail(medSizes, Image.ANTIALIAS)
-                im.save(filePathName + '_med' + ext, "JPEG")
-        #f.close()
+        # if self.img2.name <> '':
+        #     imgPath = settings.MEDIA_ROOT + self.img2.name
+        #     im = Image.open(imgPath)
+        #     filePathName, ext = os.path.splitext(imgPath)
+        #     imgSize = im.size
+        #
+        #     #med image
+        #     if (imgSize[0] > medSizes[0]) or (imgSize[1] > medSizes[1]):
+        #         im.thumbnail(medSizes, Image.ANTIALIAS)
+        #         im.save(filePathName + '_med' + ext, "JPEG")
+        #
+        # if self.img3.name <> '':
+        #     imgPath = settings.MEDIA_ROOT + self.img3.name
+        #     im = Image.open(imgPath)
+        #     filePathName, ext = os.path.splitext(imgPath)
+        #     imgSize = im.size
+        #
+        #     #med image
+        #     if (imgSize[0] > medSizes[0]) or (imgSize[1] > medSizes[1]):
+        #         im.thumbnail(medSizes, Image.ANTIALIAS)
+        #         im.save(filePathName + '_med' + ext, "JPEG")
 
     def __unicode__ (self):
         return self.title
-
-    class Admin:
-        list_display = ('title', 'description', 'createdDate', 'publishContent' )
-        list_filter = ['publishContent']
-        search_fields = ['title', 'description']
