c6e2377c0e25b0888c4707ffd44656ae2289a48f
José Ignacio Aguilera <jiaguilera@gmail.com>
2017-03-10 21:55:28 -0300

    cambios

--- contenidos/views.py
+++ contenidos/views.py
@@ -182,7 +182,7 @@ def send_mail(request):
     data = { u'sent': u'no' }
 
     try:
-        toaddrs = ','.join(settings.EMAIL_NOTIFICATION_ACCOUNTS)
+        toaddrs = settings.EMAIL_NOTIFICATION_ACCOUNTS
         gmail_user = settings.EMAIL_ACCOUNT_USER
         gmail_pwd = settings.EMAIL_ACCOUNT_PSW
 
@@ -206,7 +206,7 @@ def send_mail(request):
             msg = MIMEText(text.encode('utf-8'), _charset='utf-8')
             msg['Subject'] = "Nuevo mensaje registrado desde cedirsalud.com.ar"
             msg['From'] = gmail_user
-            msg['To'] = toaddrs
+            msg['To'] = ','.join(toaddrs)
 
             smtpserver = smtplib.SMTP("smtp.gmail.com",587)
             smtpserver.ehlo()
