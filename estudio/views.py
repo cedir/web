54e590d8144eaf9d5c059b45d93588a898bd0e15
Walter Brunetti <walterbrunetti@gmail.com>
2017-07-24 20:03:45 -0300

    Se recupera codigo perdido de API estudios

--- estudio/views.py
+++ estudio/views.py
@@ -1,6 +1,7 @@
-import django_filters
 from django.http import HttpResponse
+from rest_framework import filters
 from rest_framework import viewsets, generics
+from rest_framework.pagination import PageNumberPagination
 from estudio.models import Estudio
 from estudio.serializers import EstudioSerializer
 from imprimir import generar_informe
@@ -16,16 +17,45 @@ def imprimir(request, id_estudio):
     return generar_informe(response, estudio)
 
 
-#class EstudioFilter(django_filters.FilterSet):
-#    fecha_desde = django_filters.DateFilter(name="fecha", lookup_expr='gte')
-#    fecha_hasta = django_filters.DateFilter(name="fecha", lookup_expr='lte')
-#    class Meta:
-#        model = Estudio
-#        fields = [u'fecha', u'paciente', u'practica', u'obra_social', u'medico', u'medico_solicitante', u'fecha_desde', u'fecha_hasta']
+class EstudioPacienteFilterBackend(filters.BaseFilterBackend):
+    """
+    Filtro de estudios por paciente
+    """
+    def filter_queryset(self, request, queryset, view):
+        apellido = request.query_params.get(u'paciente_apellido')
+        nombre = request.query_params.get(u'paciente_nombre')
+        paciente_id = request.query_params.get(u'paciente_id')
+        if apellido:
+            queryset = queryset.filter(paciente__apellido__icontains=apellido)
+        if nombre:
+            queryset = queryset.filter(paciente__nombre__icontains=nombre)
+        if paciente_id:
+            queryset = queryset.filter(paciente_id=paciente_id)
+        return queryset
+
+
+class EstudioFechaFilterBackend(filters.BaseFilterBackend):
+    """
+    Filtro de estudios por fecha
+    """
+    def filter_queryset(self, request, queryset, view):
+        fecha_desde = request.query_params.get(u'fecha_desde')
+        fecha_hasta = request.query_params.get(u'fecha_hasta')
+        if fecha_desde:
+            queryset = queryset.filter(fecha=fecha_desde)
+        return queryset
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 
 class EstudioViewSet(viewsets.ModelViewSet):
     model = Estudio
-    queryset = Estudio.objects.all()[:20]
+    queryset = Estudio.objects.all()
     serializer_class = EstudioSerializer
-    #filter_class = EstudioFilter
+    filter_backends = (EstudioPacienteFilterBackend, EstudioFechaFilterBackend, )
+    pagination_class = StandardResultsSetPagination
+
