a6f6c00b15ae1e6eab62beaae1de02f2b62aa99a
Walter Brunetti <walterbrunetti@gmail.com>
2018-07-29 20:22:14 -0300

    Se agrega paciente al admin

--- paciente/admin.py
+++ paciente/admin.py
@@ -3,14 +3,10 @@ from paciente.models import Paciente
 
 
 class PacienteAdmin(admin.ModelAdmin):
-    search_fields = [u'apellido', u'nombre', 'dni']
+    search_fields = [u'apellido', u'nombre', u'dni']
     list_display = (u'nombre', u'apellido', u'telefono', u'fechaNacimiento', u'email')
-
-    def has_add_permission(self, request):
-        return False
 
     def has_delete_permission(self, request, obj=None):
         return False
 
 admin.site.register(Paciente, PacienteAdmin)
-
--- paciente/models.py
+++ paciente/models.py
@@ -4,15 +4,15 @@ from datetime import date
 
 class Paciente(models.Model):
     id = models.AutoField(primary_key=True)
-    dni = models.IntegerField()
+    dni = models.IntegerField(unique=True)
     nombre = models.CharField(u"Nombre", max_length=200, db_column=u"nombres")
     apellido = models.CharField(u"Apellido", max_length=200)
-    edad = models.IntegerField()
-    fechaNacimiento = models.DateField(null=True, blank=True)
+    edad = models.IntegerField(blank=True)
+    fechaNacimiento = models.DateField()
     domicilio = models.CharField(u"Domicilio", max_length=200, db_column=u"direccion")
     telefono = models.CharField(u"Telefono", max_length=200, db_column=u"tel")
     sexo = models.CharField(max_length=50)
-    nroAfiliado = models.CharField(u"Nro", max_length=200)
+    nroAfiliado = models.CharField(u"Nro Afiliado", max_length=200)
     email = models.CharField(u'Email', max_length=200, db_column=u"e_mail")
 
     @property
