cdab130e23de43aafacf671115f088b0913e9de4
fede <federicobadaloni@hotmail.com>
2018-09-25 18:46:08 -0300

    alta, búsqueda y modificación de obras sociales en admin

--- obra_social/admin.py
+++ obra_social/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import ObraSocial
 
-# Register your models here.
+class ObraSocialAdmin(admin.ModelAdmin):
+    actions = None
+    search_fields = [u'nombre']
+    list_display = (u'nombre', u'telefono', u'localidad', u'direccion', u'codigo_postal', u'observaciones')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(ObraSocial, ObraSocialAdmin)
