898763e05b2ff567fb10848d4fb0cc7e54d6373e
fede <federicobadaloni@hotmail.com>
2018-10-04 12:35:20 -0300

    Borre el comment que no aportaba mucho.

--- practica/admin.py
+++ practica/admin.py
@@ -4,12 +4,6 @@ from models import Practica
 
 
 class PracticaAdmin(admin.ModelAdmin):
-    '''
-    Las prácticas se pueden buscar por nombre o abreviatura, pero la abreviatura no aparece en el listado para mantenerlo limpio.
-    Se pueden buscar y agregar y modificar pero no borrar.
-
-    Fede@Septiembre 2018
-    '''
     actions = None
     fields = (u'descripcion', u'abreviatura', u'codigoMedico', u'codigo_medico_osde')
     search_fields = [u'descripcion', u'abreviatura']
