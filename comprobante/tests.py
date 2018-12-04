607f50030d3e044a2be8d973ac575312d5805f07
fede <federicobadaloni@hotmail.com>
2018-12-04 20:51:15 -0300

    test de calculador informe

--- comprobante/tests.py
+++ comprobante/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Comprobante, TipoComprobante
+from .calculador_informe import calculador_informe_factory, CalculadorInformeFactura, CalculadorInformePresentacion
 
-# Create your tests here.
+
+class CreateInformeFactoryTest(TestCase):
+    def setUp(self):
+        self.tipo_comprobante_liquidacion = TipoComprobante.objects.create(nombre='Liquidacion')
+        self.tipo_comprobante_factura = TipoComprobante.objects.create(nombre='Factura')
+
+    def test_factory_success(self):
+        comprobante = Comprobante.objects.create(tipo=self.tipo_comprobante_liquidacion)
+        instance_created = calculador_informe_factory(comprobante)
+        assert isinstance(instance_created, CalculadorInformeFactura)
+
+        comprobante = Comprobante.objects.create(tipo=self.tipo_comprobante_factura)
+        instance_created = calculador_informe_factory(comprobante)
+        assert isinstance(instance_created, CalculadorInformePresentacion)
