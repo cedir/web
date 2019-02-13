83615ba5ee18319225c10f97646b9df72e4e06f6
Walter Brunetti <walterbrunetti@gmail.com>
2019-02-12 21:20:52 -0300

    se cambia order_by por default

--- caja/views.py
+++ caja/views.py
@@ -6,6 +6,6 @@ from common.drf.views import StandardResultsSetPagination
 
 class MovimientoCajaViewSet(viewsets.ModelViewSet):
     model = MovimientoCaja
-    queryset = MovimientoCaja.objects.all()
+    queryset = MovimientoCaja.objects.all().order_by('-id')
     serializer_class = MovimientoCajaFullSerializer
     pagination_class = StandardResultsSetPagination
