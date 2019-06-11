8951407b8c62eb26175dd3815a0161808ff56235
QPotato <federicobadaloni@hotmail.com>
2019-06-10 21:18:24 -0300

    tests

--- comprobante/tests.py
+++ comprobante/tests.py
@@ -2,9 +2,11 @@
 from datetime import datetime
 from decimal import Decimal
 from django.test import TestCase
+from mock import patch, Mock
 from comprobante.models import Comprobante, TipoComprobante
 from comprobante.calculador_informe import calculador_informe_factory, CalculadorInformeFactura, CalculadorInformeNotaCredito, CalculadorInformeNotaDebito
-
+from comprobante.afip import Afip, AfipErrorValidacion, AfipErrorRed
+from httplib2 import ServerNotFoundError
 
 class CreateInformeFactoryTest(TestCase):
     def setUp(self):
@@ -104,3 +106,96 @@ class TestCaluloRetencionCedir(TestCase):
         calculador = calculador_informe_factory(comprobante)
         retencion_esperada = Decimal("25.00") * presentacion.total_facturado / Decimal("100.00")
         self.assertEquals(calculador.retencion_cedir, retencion_esperada)
+
+
+TICKET = "ticket"
+
+class TestAfipAPI(TestCase):
+    '''
+    Test sobre la clase AFIP.
+    '''
+
+    @patch("comprobante.afip.WSMTXCA")
+    def test_error_de_conexion_en_constructor_lanza_excepcion(self, mock_wsmtxca):
+        mock_wsmtxca.return_value.Conectar.side_effect = ServerNotFoundError
+        with self.assertRaises(AfipErrorRed):
+            Afip("", "", 1)
+
+    @patch("comprobante.afip.WSAA")
+    @patch("comprobante.afip.WSMTXCA")
+    def test_error_de_conexion_lanza_excepcion(self, mock_wsmtxca, mock_wsaa):
+        mock_wsaa.return_value.Autenticar.return_value = TICKET
+        mock_wsmtxca.return_value.Conectar.return_value = True
+        mock_wsmtxca.return_value.CompUltimoAutorizado.return_value = 0
+        mock_wsmtxca.return_value.AgregarIva.return_value = None
+        mock_wsmtxca.return_value.CAESolicitar.side_effect = ServerNotFoundError
+        
+        afip = Afip("", "", 1)
+        with self.assertRaises(AfipErrorRed):
+            afip.emitir_comprobante([], {}, [], tipo_cbte=1, punto_vta=1)
+
+
+    @patch("comprobante.afip.WSAA")
+    @patch("comprobante.afip.WSMTXCA")
+    def test_comprobante_rechazado_por_webservice_lanza_excepcion(self, mock_wsmtxca, mock_wsaa):
+        mock_wsaa.return_value.Autenticar.return_value = TICKET
+        mock_wsmtxca.return_value.Conectar.return_value = True
+        mock_wsmtxca.return_value.CompUltimoAutorizado.return_value = 0
+        mock_wsmtxca.return_value.AgregarIva.return_value = None
+        mock_wsmtxca.return_value.CAESolicitar.return_value = None
+
+        mock_wsmtxca.return_value.Resultado = "R"
+        
+        afip = Afip("", "", 1)
+        with self.assertRaises(AfipErrorValidacion):
+            afip.emitir_comprobante([], {}, [], tipo_cbte=1, punto_vta=1)
+    
+    @patch("comprobante.afip.WSAA")
+    @patch("comprobante.afip.WSMTXCA")
+    def test_comprobante_aceptado_por_weservice_devuelve_dict(self, mock_wsmtxca, mock_wsaa):
+        mock_wsaa.return_value.Autenticar.return_value = TICKET
+        mock_wsmtxca.return_value.Conectar.return_value = True
+        mock_wsmtxca.return_value.CompUltimoAutorizado.return_value = 0
+        mock_wsmtxca.return_value.AgregarIva.return_value = None
+        mock_wsmtxca.return_value.CAESolicitar.return_value = None
+
+        mock_wsmtxca.return_value.Resultado = "A"
+
+        afip = Afip("", "", 1)
+        self.assertTrue(isinstance(afip.emitir_comprobante([], {}, [], tipo_cbte=1, punto_vta=1), dict))
+            
+
+    @patch("comprobante.afip.WSAA", autospec=True)
+    @patch("comprobante.afip.WSMTXCA")
+    def test_ticket_expirado_renueva_y_emite(self, mock_wsmtxca, mock_wsaa):
+        mock_wsaa.return_value.Expirado.side_effect = [True, False]
+        mock_wsaa.return_value.Autenticar.side_effect = [TICKET, TICKET]
+        mock_wsmtxca.return_value.Conectar.return_value = True
+        mock_wsmtxca.return_value.CompUltimoAutorizado.return_value = 0
+        mock_wsmtxca.return_value.AgregarIva.return_value = None
+        mock_wsmtxca.return_value.CAESolicitar.return_value = None
+
+        mock_wsmtxca.return_value.Resultado = "A"
+
+        afip = Afip("", "", 1)
+        afip.emitir_comprobante([], {}, [], tipo_cbte=1, punto_vta=1)
+        self.assertEquals(mock_wsaa.return_value.Autenticar.call_count, 2)
+        mock_wsmtxca.return_value.CAESolicitar.assert_is_called()
+
+
+    @patch("comprobante.afip.WSAA")
+    @patch("comprobante.afip.WSMTXCA")
+    def test_ticket_valido_no_renueva_y_emite(self, mock_wsmtxca, mock_wsaa):
+        mock_wsaa.return_value.Autenticar.return_value = TICKET
+        mock_wsaa.return_value.Expirado.return_value = False
+        mock_wsmtxca.return_value.Conectar.return_value = True
+        mock_wsmtxca.return_value.CompUltimoAutorizado.return_value = 0
+        mock_wsmtxca.return_value.AgregarIva.return_value = None
+        mock_wsmtxca.return_value.CAESolicitar.return_value = None
+
+        mock_wsmtxca.return_value.Resultado = "A"
+
+        afip = Afip("", "", 1)
+        afip.emitir_comprobante([], {}, [], tipo_cbte=1, punto_vta=1)
+        self.assertEquals(mock_wsaa.return_value.Autenticar.call_count, 1)
+        mock_wsmtxca.return_value.CAESolicitar.assert_is_called()
