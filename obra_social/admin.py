1a33e1998bd3b8b99aef1e168b781e18de74d705
fede <federicobadaloni@hotmail.com>
2019-08-28 19:35:00 -0300

    Arancel Obra Social
    - Se agrega el model nuevo
    - Se debe correr en postgres: LTER TABLE "AlmacenPreciosEstOS" ADD COLUMN "id" bigint;
    - La migration se debe fakear
    - Editar un arancel viejo falla, porque no tienen IDs.

--- obra_social/admin.py
+++ obra_social/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import ObraSocial
+from models import ObraSocial, ArancelObraSocial
 
 class ObraSocialAdmin(admin.ModelAdmin):
     actions = None
@@ -10,3 +10,13 @@ class ObraSocialAdmin(admin.ModelAdmin):
         return False
 
 admin.site.register(ObraSocial, ObraSocialAdmin)
+
+class ArancelObraSocialAdmin(admin.ModelAdmin):
+    actions = None
+    search_fields = [u'practica', u'obra_social']
+    list_display = (u'practica', u'obra_social', u'precio', u'precio_anestesia', u'fecha')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(ArancelObraSocial, ArancelObraSocialAdmin)
