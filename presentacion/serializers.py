3744f3d38707cb581fd923ee8f8c65c4295f0edd
Walter Brunetti <walterbrunetti@gmail.com>
2019-09-30 18:40:32 -0300

    nuevos endpoints para presentacion retreive en sus dos formas

--- presentacion/serializers.py
+++ presentacion/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from presentacion.models import Presentacion
+from estudio.models import Estudio
 from obra_social.serializers import ObraSocialSerializer
 from comprobante.serializers import ComprobanteSerializer, ComprobanteSmallSerializer
 
@@ -25,3 +26,43 @@ class PresentacionSerializer(serializers.ModelSerializer):
 
     def get_estado(self, obj):
         return Presentacion.ESTADOS[obj.estado][1]
+
+
+class EstudioSerializer(serializers.ModelSerializer):
+    obra_social = ObraSocialSerializer()
+
+    class Meta:
+        model = Estudio
+        fields = (u'id', u'fecha', u'paciente', u'practica', u'obra_social', u'medico',)
+
+
+class PresentacionRetrieveSerializer(serializers.ModelSerializer):
+    obra_social = ObraSocialSerializer()
+    comprobante = ComprobanteSerializer()
+    estado = serializers.SerializerMethodField()
+    estudios = EstudioSerializer(many=True)
+
+    class Meta:
+        model = Presentacion
+
+    def get_estado(self, obj):
+        return Presentacion.ESTADOS[obj.estado][1]
+
+
+class EstudioEnPresentacionSerializer(serializers.Serializer):
+    estudio_id = serializers.IntegerField()
+    importe = serializers.DecimalField(max_digits=16, decimal_places=2)
+
+
+class PresentacionCreateUpdateSerializer(serializers.Serializer):
+    """
+    {"fecha": 11, "estudios": [{"estudio_id": 22, "importe": 22.22}, ...]}
+    o puede ser
+
+    {"fecha": 11, estudios: [{id_estudio: {pension: 12, importe: 12, medicacion:1}}, ...]
+
+    """
+    id_presentacion = serializers.IntegerField()
+    # obra social
+    # fecha?
+    estudios = EstudioEnPresentacionSerializer(many=True)
--- presentacion/views.py
+++ presentacion/views.py
@@ -1,10 +1,12 @@
 import simplejson
 from django.http import HttpResponse
+from rest_framework.response import Response
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route
+from rest_framework.decorators import detail_route, list_route
 from common.drf.views import StandardResultsSetPagination
+from estudio.models import Estudio
 from presentacion.models import Presentacion
-from presentacion.serializers import PresentacionSerializer
+from presentacion.serializers import PresentacionSerializer, PresentacionRetrieveSerializer, PresentacionCreateUpdateSerializer
 from presentacion.obra_social_custom_code.osde_presentacion_digital import \
     OsdeRowEstudio, OsdeRowMedicacion, OsdeRowPension, OsdeRowMaterialEspecifico
 from presentacion.obra_social_custom_code.amr_presentacion_digital import AmrRowEstudio
@@ -16,6 +18,42 @@ class PresentacionViewSet(viewsets.ModelViewSet):
     filter_fields = ('obra_social',)
     pagination_class = StandardResultsSetPagination
     page_size = 50
+
+    """
+    POST presentection/  --> crea presentacion
+    PATCH presentacion/123   -> finalizar  (validar que esta en estado abierta, sino no se puede modificar)
+    PATCH presentacion/123/cobrar  -->validar que esta en estado pendiente
+    PATCH presentacion/123/abrir  -->validar que esta en estado pendiente (anula comprobante y cambia estado, etc)
+    GET presentacion/20  --> este se puede usar para cargar cualqueir pantalla (cobro, ver detalle, actulizar presentacion abierta)
+    GET presenctacion/get_estudios_sin_presentar
+    LIST presentacion
+    """
+
+    serializers = {
+        'retrieve': PresentacionRetrieveSerializer,
+        'create': PresentacionCreateUpdateSerializer,
+        'update': PresentacionCreateUpdateSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.serializers.get(self.action, self.serializer_class)
+
+    def perform_create(self, serializer):
+        pass
+
+    def perform_update(self, serializer):
+        pass
+
+    @list_route(methods=['get'])
+    def get_estudios_sin_presentar(self, request):
+        #presentacion = Presentacion(obra_social_id=2)
+        #presentacion.estudios = Estudio.objects.filter()[:10]
+
+        # TODO: ver como hacer para usar el mismo serializar tanto para GET como para este metodo donde no hay una instancia
+        presentacion = Presentacion.objects.filter(estado=2).last()
+        serializer = PresentacionRetrieveSerializer(presentacion)
+
+        return Response(serializer.data)
 
     @detail_route(methods=['get'])
     def get_detalle_osde(self, request, pk=None):
