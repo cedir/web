aff3fcf41e190db7b14d97caba8b6e8675f93e56
DanielLezcano727 <daniel.lezcano727@gmail.com>
2019-11-03 07:16:02 -0300

    Añadidos los tests que deben dar positivo

--- comprobante/test_comprobantes_asociados.py
+++ comprobante/test_comprobantes_asociados.py
@@ -1,14 +1,45 @@
 from django.test import TestCase
-from comprobante.models import Comprobante
+from comprobante.models import Comprobante, TipoComprobante, \
+    ID_TIPO_COMPROBANTE_FACTURA, ID_TIPO_COMPROBANTE_FACTURA_CREDITO_ELECTRONICA, \
+    ID_TIPO_COMPROBANTE_NOTA_DE_CREDITO, ID_TIPO_COMPROBANTE_NOTA_DE_CREDITO_ELECTRONICA, \
+    ID_TIPO_COMPROBANTE_NOTA_DE_DEBITO, ID_TIPO_COMPROBANTE_NOTA_DE_DEBITO_ELECTRONICA
 
 HTTP_BAD_REQUEST = 400
+HTTP_OK = 200
+NUMERO_TERMINAL = 91
 
 class TestComprobantesAsociados(TestCase):
 
     fixtures = ['comprobantes.json']
 
-    def test_crear_comprobante_asociado(self):
-        pass
+    def test_crear_nota_de_credito_asociada_a_factura_valido(self):
+        comp = Comprobante.objects.get(pk = 1)
+        comp2 = Comprobante.objects.get(pk = 2)
+
+        assert HTTP_OK == crear_comprobante_asociado(comp, 500, ID_TIPO_COMPROBANTE_NOTA_DE_CREDITO)
+        assert HTTP_OK == crear_comprobante_asociado(comp2, 500, ID_TIPO_COMPROBANTE_NOTA_DE_CREDITO)
+
+    def test_crear_nota_de_debito_asociada_a_factura_valido(self):
+        comp = Comprobante.objects.get(pk = 1)
+        comp2 = Comprobante.objects.get(pk = 2)
+
+        assert HTTP_OK == crear_comprobante_asociado(comp, 500, ID_TIPO_COMPROBANTE_NOTA_DE_DEBITO)
+        assert HTTP_OK == crear_comprobante_asociado(comp2, 1200, ID_TIPO_COMPROBANTE_NOTA_DE_DEBITO)
+
+    def test_crear_nota_de_credito_electronica_asociada_a_factura_electronica_valido(self):
+        comp = Comprobante.objects.get(pk = 1)
+        comp2 = Comprobante.objects.get(pk = 2)
+
+        assert HTTP_OK == crear_comprobante_asociado(comp, 500, ID_TIPO_COMPROBANTE_NOTA_DE_CREDITO_ELECTRONICA)
+        assert HTTP_OK == crear_comprobante_asociado(comp2, 1200, ID_TIPO_COMPROBANTE_NOTA_DE_CREDITO_ELECTRONICA)
+
+
+    def test_crear_nota_de_debito_electronica_asociada_a_factura_valido(self):
+        comp = Comprobante.objects.get(pk = 1)
+        comp2 = Comprobante.objects.get(pk = 2)
+
+        assert HTTP_OK == crear_comprobante_asociado(comp, 500, ID_TIPO_COMPROBANTE_NOTA_DE_DEBITO_ELECTRONICA)
+        assert HTTP_OK == crear_comprobante_asociado(comp2, 1200, ID_TIPO_COMPROBANTE_NOTA_DE_DEBITO_ELECTRONICA)
 
     def test_obtener_comprobante_con_id_valido(self):
         
@@ -20,9 +51,3 @@ class TestComprobantesAsociados(TestCase):
     def test_obtener_comprobante_con_id_invalido(self):
         assert obtener_comprobante_id(232) == HTTP_BAD_REQUEST 
         assert obtener_comprobante_id(1000) == HTTP_BAD_REQUEST 
-
-    def test_crear_comprobante(self):
-        pass
-
-    def test_crear_comprobante_afip(self):
-        pass
