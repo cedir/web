bf9ca0264adc10a4fbf098f516d48aff4174a1b4
QPotato <federicobadaloni@hotmail.com>
2019-12-02 12:06:22 -0300

    API estudios_sin_presentar

--- obra_social/tests.py
+++ obra_social/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+
+from estudio.models import Estudio
+
+class TestDetallesObrasSociales(TestCase):
+    fixtures = ['pacientes.json', 'medicos.json', 'practicas.json', 'obras_sociales.json', 'anestesistas.json', 'presentaciones.json', 'comprobantes.json', 'estudios.json']
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='test', is_superuser=True)
+        self.client = Client(HTTP_GET='localhost')
+        self.client.login(username='test', password='test')
+
+    def test_detalle_osde(self):
+        estudio = Estudio.objects.get(pk=9)
+        assert estudio.presentacion is None
+        assert estudio.obra_social.pk == 1
+        response = self.client.get('/api/obra_social/1/estudios_sin_presentar/')
+        estudios_response = json.loads(response.content)
+        assert len(estudios_response) > 0
--- obra_social/views.py
+++ obra_social/views.py
@@ -1,7 +1,10 @@
+import simplejson
 from django.http import HttpResponse
-from rest_framework import filters
-from rest_framework import viewsets, generics
-from estudio.models import ObraSocial
+from rest_framework import filters, viewsets, generics
+from rest_framework.decorators import detail_route
+from estudio.models import Estudio
+from obra_social.models import ObraSocial
+from estudio.serializers import EstudioDePresetancionSerializer
 from obra_social.serializers import ObraSocialSerializer
 
 class ObraSocialNombreFilterBackend(filters.BaseFilterBackend):
@@ -12,7 +15,7 @@ class ObraSocialNombreFilterBackend(filters.BaseFilterBackend):
         nombre = request.query_params.get(u'nombre')
         if nombre:
             queryset = queryset.filter(nombre__icontains=nombre)
-            
+
             return queryset
 
 # Create your views here.
@@ -22,3 +25,19 @@ class ObraSocialViewSet(viewsets.ModelViewSet):
     serializer_class = ObraSocialSerializer
     filter_backends = (ObraSocialNombreFilterBackend, )
     pagination_class = None
+
+    @detail_route(methods=['get'])
+    def estudios_sin_presentar(self, request, pk=None):
+        estudios = Estudio.objects.filter(
+            obra_social__pk=pk,
+            presentacion__pk=0,
+            es_pago_contra_factura=0,
+        ).order_by('fecha', 'id')
+        try:
+            response = HttpResponse(simplejson.dumps([
+                EstudioDePresetancionSerializer(estudio).data
+                for estudio in estudios
+            ]), content_type='application/json')
+        except Exception as ex:
+            response = HttpResponse(simplejson.dumps({'error': ex.message}), status=500, content_type='application/json')
+        return response
