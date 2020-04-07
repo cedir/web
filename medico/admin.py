0f75d18eb0c8ab26396e7732801240da3de4773b
Daniel Lezcano <daniel.lezcano727@gmail.com>
2020-04-07 10:10:48 -0300

    Se saca los campos de facturar a otro medico de la visualizacion del admin

--- medico/admin.py
+++ medico/admin.py
@@ -5,7 +5,7 @@ class MedicoAdmin(admin.ModelAdmin):
     actions = None
 
     search_fields = [u'apellido', u'nombre', u'matricula']
-    list_display = (u'apellido', u'nombre', u'telefono', u'domicilio', u'localidad', u'mail', u'matricula', u'responsabilidad_fiscal', u'clave_fiscal', u'facturar_amr_en_nombre_de_medico', u'facturar_osde_en_nombre_de_medico')
+    list_display = (u'apellido', u'nombre', u'telefono', u'domicilio', u'localidad', u'mail', u'matricula', u'responsabilidad_fiscal', u'clave_fiscal')
 
     def has_delete_permission(self, request, obj=None):
         return False
