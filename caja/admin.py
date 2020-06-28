68b112a70a394fab14ba87f0687a51236073d864
Daniel Lezcano <daniel.lezcano727@gmail.com>
2020-06-28 13:30:36 -0300

    Se agrega el formulario para poder realizar movimientos de caja y se eliminan los permisos para cambiar el formulario

--- caja/admin.py
+++ caja/admin.py
@@ -6,18 +6,22 @@ class MovimientoCajaAdmin(admin.ModelAdmin):
     actions = None
     fields = (u'fecha', u'hora', u'monto', u'concepto')
     search_fields = [u'tipo', u'concepto']
-    list_display = (u'fecha', u'hora', u'monto', u'concepto')
-
-    def has_add_permission(self, request):
-        return False
+    list_display = (u'fecha', u'hora', u'monto', u'monto_acumulado', u'concepto')
 
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = []
+        if obj:
+            readonly_fields = list(self.list_display)
+        return readonly_fields
+
     def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
-        extra_context = extra_context or {}
-        extra_context['show_save_and_continue'] = False
-        extra_context['show_save'] = False
+        if object_id:
+            extra_context = extra_context or {}
+            extra_context['show_save_and_continue'] = False
+            extra_context['show_save'] = False
         return super(MovimientoCajaAdmin, self).changeform_view(request, object_id, extra_context=extra_context)
 
 
