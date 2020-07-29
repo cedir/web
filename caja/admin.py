90b57698c6b6607c1d6ff3e2495fef57e0629359
Daniel Lezcano <daniel.lezcano727@gmail.com>
2020-07-29 14:58:46 -0300

    Se actualiza el monto acumulado y se permite asociar con un estudio

--- caja/admin.py
+++ caja/admin.py
@@ -4,17 +4,23 @@ from caja.models import MovimientoCaja
 # Register your models here.
 class MovimientoCajaAdmin(admin.ModelAdmin):
     actions = None
-    fields = (u'fecha', u'hora', u'monto', u'concepto')
+    fields = (u'fecha', u'hora', u'monto', u'concepto', u'estudio')
     search_fields = [u'tipo', u'concepto']
-    list_display = (u'fecha', u'hora', u'monto', u'monto_acumulado', u'concepto')
+    list_display = (u'fecha', u'hora', u'monto', u'monto_acumulado', u'concepto', u'estudio')
+    raw_id_fields = (u'estudio', )
 
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.monto_acumulado = obj.monto + MovimientoCaja.objects.last().monto_acumulado
+        super(MovimientoCajaAdmin, self).save_model(request, obj, form, change)
+
     def get_readonly_fields(self, request, obj=None):
         readonly_fields = []
         if obj:
-            readonly_fields = list(self.list_display)
+            readonly_fields = list(self.fields)
         return readonly_fields
 
     def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
