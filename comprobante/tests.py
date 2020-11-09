ce047c29bb9fbef194f501a356afbe320f1a5e46
Daniel Lezcano <daniel.lezcano727@gmail.com>
2020-11-09 08:57:48 -0300

    Tests sobre LineaDeComprobante

--- comprobante/tests.py
+++ comprobante/tests.py
@@ -1,30 +1,56 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from .serializers import CrearComprobanteSerializer
+from decimal import Decimal
+
+from .models import LineaDeComprobante
+from .serializers import CrearComprobanteAFIPSerializer, CrearLineaDeComprobanteSerializer
 
 class TestPostComprobantes(TestCase):
+    fixtures = ['comprobantes.json']
+
     def setUp(self):
         self.user = User.objects.create_user(username='test', password='test', is_superuser=True)
         self.cliente = Client(HTTP_GET='localhost')
         self.client.login(username='test', password='test')
+        self.linea_data = {
+            'concepto': 'hola',
+            'porcentaje_iva': '10.5',
+            'importe_neto': '500'
+        }
 
-    def test_serializer_crear_comprobante_crea_objeto_comprobantes(self):
-        c = CrearComprobanteSerializer(data={
-            'tipo_comprobante_id': '1',
-            'sub_tipo': 'B',
-            'responsable': 'Cedir',
-            'gravado_id': '2',
-            'nombre_cliente': 'Homero J. Simpson',
-            'domicilio_cliente': 'Avenida Siempreviva 742',
-            'nro_cuit': '20420874120',
-            'condicion_fiscal': 'CONSUMIDOR FINAL',
-            'lineas': [
-                { 'concepto': 'Esta es una linea', 'importeNeto': '500'},
-                { 'concepto': 'Esta es una linea', 'importeNeto': '511.11'}
-            ],
-            'neto': 0,
-        })
-        if c.is_valid():
-            print('Era valido')
-        c = c.save()
-        print(c)
+
+    def test_crear_linea_serializer_crea_objeto_linea(self):
+        linea_serializer = CrearLineaDeComprobanteSerializer(data=self.linea_data)
+        
+        assert isinstance(linea_serializer, CrearLineaDeComprobanteSerializer)
+        assert linea_serializer.is_valid()
+        
+        linea_db = linea_serializer.save()
+        
+        assert isinstance(linea_db, LineaDeComprobante)
+        assert linea_db.concepto == self.linea_data['concepto']
+        assert linea_db.importe_neto == Decimal(self.linea_data['importe_neto'])
+
+    def test_crear_linea_serializer_calcula_correctamente_iva(self):
+        linea_serializer = CrearLineaDeComprobanteSerializer(data=self.linea_data)
+        
+        assert linea_serializer.is_valid()
+        
+        linea = linea_serializer.save()
+        importe_neto = Decimal(self.linea_data['importe_neto'])
+        porcentaje_iva = Decimal(self.linea_data['porcentaje_iva'])
+        
+        assert linea.iva == importe_neto * porcentaje_iva / Decimal(100)
+
+    def test_crear_linea_serializer_calcula_correctamente_total(self):
+        linea_serializer = CrearLineaDeComprobanteSerializer(data=self.linea_data)
+        
+        assert linea_serializer.is_valid()
+        
+        linea = linea_serializer.save()
+        importe_neto = Decimal(self.linea_data['importe_neto'])
+        porcentaje_iva = Decimal(self.linea_data['porcentaje_iva'])
+        
+        total = importe_neto + importe_neto * porcentaje_iva / Decimal(100)
+        
+        assert linea.sub_total == total
