c06622f79f504e8956e032767a2000a3b4561313
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-01-30 00:57:42 -0300

    Esqueleto tests anestesista

--- anestesista/models.py
+++ anestesista/models.py
@@ -15,7 +15,7 @@ class Anestesista(models.Model):
     id = models.AutoField(primary_key=True, db_column="idMedicoAn")
     nombre = models.CharField("Nombre", max_length=200, db_column="nombreMedicoAn")
     apellido = models.CharField("Apellido", max_length=200, db_column="apellidoMedicoAn")
-    matricula = models.CharField("Matricula", max_length=200, db_column="nroMatricula", null=True, blank=True)
+    matricula = models.CharField("Matricula", max_length=200, db_column="nroMatricula", null=True, blank=True) # Quizas deberia ser un entero
     direccion = models.CharField("Direccion", max_length=200, db_column="direccionMedico", null=True, blank=True)
     localidad = models.CharField("Localidad", max_length=200, db_column="localidadMedico", null=True, blank=True)
     telefono = models.CharField("Telefono", max_length=200, db_column="telMedico", null=True, blank=True)
--- anestesista/tests.py
+++ anestesista/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from anestesista.models import Anestesista
+from estudio.models import Estudio, ID_SUCURSAL_CEDIR, ID_SUCURSAL_HOSPITAL_ITALIANO
+
+class TestAnestesistas(TestCase):
+    fixtures = ['pacientes.json', 'medicos.json', 'practicas.json', 'obras_sociales.json',
+                'anestesistas.json', 'presentaciones.json', 'comprobantes.json', 'estudios.json', "medicamentos.json"]
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='test', is_superuser=True)
+        self.client = Client(HTTP_GET='localhost')
+        self.client.login(username='test', password='test')
+
+    def test_pago_anestesista_filtra_por_sucursal(self):
+        assert estudios.count() > 0
+        response = self.client.get(f'/api/anestesista/1/pago/2013/11/?sucursal={ID_SUCURSAL_CEDIR}')
+        print(response.content)
+        estudios = Estudio.objects.filter(anestesista__id=1, sucursal=ID_SUCURSAL_HOSPITAL_ITALIANO)
+        assert estudios.count() > 0
