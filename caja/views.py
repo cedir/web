dceed47c583893711d538c9ec0a9affe2ce557df
Julio Olivencia <julioolivencia891@gmail.com>
2021-02-17 10:58:41 -0300

    esquema crear movimiento caja

--- caja/views.py
+++ caja/views.py
@@ -5,6 +5,7 @@ from caja.serializers import MovimientoCajaFullSerializer
 from common.drf.views import StandardResultsSetPagination
 from rest_framework.filters import BaseFilterBackend
 from distutils.util import strtobool
+from typing import Dict
 
 class CajaConceptoFilterBackend(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
@@ -47,10 +48,40 @@ class CajaIncluirEstudioFilterBackend(BaseFilterBackend):
         return queryset
 
 class MovimientoCajaViewSet(viewsets.ModelViewSet):
-    model = MovimientoCaja
+    z = MovimientoCaja
     queryset = MovimientoCaja.objects.all().order_by('-id')
     serializer_class = MovimientoCajaFullSerializer
     pagination_class = StandardResultsSetPagination
     filter_backends = (CajaConceptoFilterBackend, CajaMedicoFilterBackend,
         CajaFechaFilterBackend, CajaTipoMovimientoFilterBackend,
         CajaIncluirEstudioFilterBackend)
+
+    def create(self, request):
+        try:
+            request: Dict = request.data
+            fecha = request['fecha']
+            hora = request['hora']
+            estudio = request['estudio']
+            movimientos = request['movimientos']
+            monto_acumulado = MovimientoCaja().objects.last().monto_acumulado
+            
+            for movimiento in movimientos:
+                concepto = movimiento['concepto']
+                tipo = movimiento['tipo']
+                medico = movimiento['medico']
+                monto = movimiento['monto']
+                monto_acumulado += monto
+
+                movimiento_caja = MovimientoCaja(
+                    medico = medico, estudio = estudio,
+                    concepto = concepto, fecha = fecha,
+                    hora = hora, tipo = tipo, monto = monto,
+                    monto_acumulado = monto_acumulado,
+                )
+                movimiento_caja.save()
+
+            response = JsonResponse({}, status=status.HTTP_200_OK)
+
+        except Exception as ex:
+            response = JsonResponse({'error': str(ex)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response
