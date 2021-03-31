14e62f31731ee5b4e05ea4703fa906148d6f9fde
Julio Olivencia <julioolivencia891@gmail.com>
2021-03-31 11:37:43 -0300

    se añade la funcionalidad de crear movimientos de caja

--- caja/serializers.py
+++ caja/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
+from collections import OrderedDict
 from .models import MovimientoCaja, TipoMovimientoCaja
 from estudio.models import Estudio
+from medico.models import Medico
 from practica.serializers import PracticaSerializer
 from obra_social.serializers import ObraSocialSerializer
 from paciente.serializers import PacienteSerializer
 from medico.serializers import MedicoSerializer
-
+from decimal import Decimal
+from datetime import date, datetime
 
 class TipoMovimientoCajaSerializer(serializers.ModelSerializer):
 
@@ -42,18 +46,89 @@ class MovimientoCajaFullSerializer(serializers.ModelSerializer):
         model = MovimientoCaja
         fields = ('id', 'concepto', 'estudio', 'monto', 'monto_acumulado', 'fecha', 'hora', 'tipo', 'medico')
 
-class MovimientoCajaCamposVariablesSerializer(serializers.ModelSerializer):
-    tipo = TipoMovimientoCajaSerializer()
-    medico = MedicoSerializer()
-    
-    class Meta:
-        model = MovimientoCaja
-        fields = ('hora', 'concepto', 'tipo', 'medico', 'monto')
+class MovimientoCajaCamposVariablesSerializer(serializers.Serializer):
+    tipo_id = serializers.IntegerField()
+    medico_id = serializers.IntegerField(required=False)
+    concepto = serializers.CharField(required=False) 
+    monto = serializers.DecimalField(16, 2, required=True)
+
+    def to_internal_value(self, data):
+        data['tipo_id'] = self.validate_tipo_id(data['tipo_id'])
+        data['medico_id'] = self.validate_medico_id(data['medico_id'])
+        data['monto'] = self.validate_monto(data['monto'])
+        return data
+
+    def validate_medico_id(self, value):
+        try:
+            if value:
+                value = Medico.objects.get(pk=value)
+            else:
+                value = None
+        except Medico.DoesNotExist:
+            raise ValidationError("id de medico invalida")
+        return value
+
+    def validate_monto(self, value):
+        if not value:
+            raise ValidationError("no existe monto")
+        try:
+            value = Decimal(value)
+        except Decimal.InvalidOperation:
+            raise ValidationError("el monto no es un numero")
+        return value
+
+    def validate_tipo_id(self, value):
+        if not value:
+            raise ValidationError("no existe tipo de movimiento")
+        try:
+            value = TipoMovimientoCaja.objects.get(pk=value)
+        except TipoMovimientoCaja.DoesNotExist:
+            raise ValidationError("tipo de movimiento invalido")
+        return value
+
+    def create(self, validated_data):
+        return validated_data
 
 class MovimientoCajaCreateSerializer(serializers.ModelSerializer):
-    estudio = EstudioCajaSerializer()
+    estudio_id = serializers.IntegerField(required=False)
     movimientos = MovimientoCajaCamposVariablesSerializer(many=True)
 
     class Meta:
         model = MovimientoCaja
-        fields = ('id', 'estudio', 'fecha', 'movimientos')
+        fields = ('estudio_id', 'movimientos')
+
+    def to_internal_value(self, data):
+        data['estudio_id'] = self.validate_estudio_id(data['estudio_id'])
+        movimientos = [MovimientoCajaCamposVariablesSerializer(data=movimiento) for movimiento in data['movimientos']]
+        for movimiento in movimientos:
+            movimiento.is_valid(raise_exception=True)
+        data['movimientos'] = [movimiento.save() for movimiento in movimientos]
+        return data
+
+    def validate_estudio_id(self, value):
+        try:
+            if value:
+                value = Estudio.objects.get(pk=value)
+            else:
+                value = None
+        except Estudio.DoesNotExist:
+            raise ValidationError("id de estudio invalida")
+        return value  
+
+    def create(self, validated_data):
+        monto_acumulado = MovimientoCaja.objects.last().monto_acumulado
+        fecha = date.today()
+        hora = datetime.now()
+        estudio = validated_data['estudio_id']
+        movimientos = []
+        for movimiento in validated_data['movimientos']:
+            tipo = movimiento['tipo_id']
+            medico = movimiento['medico_id']
+            concepto = movimiento['concepto']
+            monto = movimiento['monto']
+            monto_acumulado += monto
+            movimiento = MovimientoCaja(fecha = fecha, hora = hora, estudio = estudio,
+            tipo = tipo, medico = medico, monto = monto, concepto = concepto, monto_acumulado = monto_acumulado)
+            movimiento.save()
+            movimientos += [movimiento]
+        return movimientos
--- caja/views.py
+++ caja/views.py
@@ -1,11 +1,14 @@
 # pylint: disable=no-name-in-module, import-error
-from rest_framework import viewsets
+from rest_framework import viewsets, status, serializers
+from django.http import JsonResponse
 from caja.models import MovimientoCaja
 from caja.serializers import MovimientoCajaFullSerializer, MovimientoCajaCreateSerializer
 from common.drf.views import StandardResultsSetPagination
 from rest_framework.filters import BaseFilterBackend
 from distutils.util import strtobool
 from typing import Dict
+from datetime import date, datetime
+from decimal import Decimal
 
 class CajaConceptoFilterBackend(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
@@ -61,30 +64,48 @@ class MovimientoCajaViewSet(viewsets.ModelViewSet):
 
     def create(self, request):
         try:
-            request: Dict = request.data
-            fecha = request['fecha']
-            estudio = request['estudio']
-            movimientos = request['movimientos']
-            monto_acumulado = MovimientoCaja().objects.last().monto_acumulado
-            
-            for movimiento in movimientos:
-                hora = movimiento['hora']
-                concepto = movimiento['concepto']
-                tipo = movimiento['tipo']
-                medico = movimiento['medico']
-                monto = movimiento['monto']
-                monto_acumulado += monto
+            movimientos_serializer = MovimientoCajaCreateSerializer(data=request.data)
+            if not movimientos_serializer.is_valid():
+                raise serializers.ValidationError(movimientos_serializer.errors)
 
-                movimiento_caja = MovimientoCaja(
-                    medico = medico, estudio = estudio,
-                    concepto = concepto, fecha = fecha,
-                    hora = hora, tipo = tipo, monto = monto,
-                    monto_acumulado = monto_acumulado,
-                )
-                movimiento_caja.save()
-
-            response = JsonResponse({}, status=status.HTTP_200_OK)
-
+            movimientos = movimientos_serializer.save()
+            response = JsonResponse({}, status=status.HTTP_201_CREATED)
+        except serializers.ValidationError as ex:
+            response = JsonResponse({'error': str(ex)}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as ex:
             response = JsonResponse({'error': str(ex)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
         return response
+
+    # def create(self, request):
+    #     request = MovimientoCajaCreateSerializer(request)
+    #     try:
+    #         request: Dict = request.data
+    #         fecha = date.today()
+    #         hora = datetime.now()
+    #         estudio = request['estudio']
+    #         movimientos = request['movimientos']
+    #         monto_acumulado = MovimientoCaja.objects.last().monto_acumulado
+            
+    #         for movimiento in movimientos:
+    #             concepto = movimiento['concepto']
+    #             tipo = movimiento['tipo']
+    #             medico = movimiento['medico']
+    #             monto = Decimal(movimiento['monto']) # corroborar
+    #             monto_acumulado = monto + monto_acumulado
+    #             print(medico)
+    #             movimiento_caja = MovimientoCaja(
+    #                 medico = medico, estudio = estudio,
+    #                 concepto = concepto, fecha = fecha,
+    #                 hora = hora, tipo = tipo, monto = monto,
+    #                 monto_acumulado = monto_acumulado,
+    #             )
+    #             print('aaaaaaaa')
+    #             movimiento_caja.is_valid(raise_exception=True)
+    #             movimiento_caja.save()
+
+    #         response = JsonResponse({}, status=status.HTTP_200_OK)
+
+    #     except Exception as ex:
+    #         response = JsonResponse({'error': str(ex)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    #     return response
