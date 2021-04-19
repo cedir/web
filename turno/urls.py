4089ca870edfe516c3b7922f157f869e6f539053
Daniel Lezcano <daniel.lezcano727@gmail.com>
2021-04-19 20:55:59 -0300

    Se realiza el ViewSet de turnos y el esqueleto de la funcion contador_turnos

--- turno/urls.py
+++ turno/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import include, url
 from rest_framework import routers
-from turno.views import anunciar, anular, reprogramar, confirmar, guardar, update, get_home, get_turno, get_buscar_turnos, get_turnos_disponibles, get_next_day_line, get_back_day_line, InfoTurnoViewSet
+from turno.views import anunciar, anular, reprogramar, confirmar, guardar, update, get_home, get_turno, get_buscar_turnos, get_turnos_disponibles, get_next_day_line, get_back_day_line, InfoTurnoViewSet, TurnoViewSet
 
 
 router = routers.SimpleRouter()
 router.register(r'infoturno', InfoTurnoViewSet)
+router2 = routers.SimpleRouter()
+router2.register(r'turno_d', TurnoViewSet)
 
 urlpatterns = [
     url(r'^api/turno/', include(router.urls)),
+    url(r'^api/', include(router2.urls)),
     url(r'^turnos/$', get_home),
     url(r'^turno/buscar/', get_buscar_turnos),
     url(r'^turno/disponibles/', get_turnos_disponibles),
